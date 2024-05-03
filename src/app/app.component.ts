e6d46f6985697fd44a6fa1102348a9c3cd043817
kabrunko-dev <kabrunko.dev@gmail.com>
2024-05-03 09:34:16 -0300

    refactor(feat/login): change form type and add form fields errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import environment from '../environments/env';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `<router-outlet />`,
 })
-export default class AppComponent {}
+export default class AppComponent implements OnInit {
+  constructor(@Inject(DOCUMENT) private readonly document: Document) {}
+
+  ngOnInit(): void {
+    if (environment.production) {
+      this.setManifestLinkTag();
+    }
+  }
+
+  private setManifestLinkTag(): void {
+    const manifestLink = this.document.createElement('link');
+    const headTag = this.document.querySelector('head');
+
+    manifestLink.setAttribute('rel', 'manifest');
+    manifestLink.setAttribute('href', 'manifest.json');
+
+    headTag?.appendChild(manifestLink);
+  }
+}
--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
 
@@ -7,33 +12,54 @@ import { EMPTY, catchError } from 'rxjs';
 
 import LoginService from './login.service';
 import SpinnerComponent from '../../shared/spinner.component';
-import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, NgIf, SpinnerComponent],
+  imports: [NgIf, ReactiveFormsModule, SpinnerComponent],
   providers: [LoginService],
   templateUrl: 'login.component.html',
   styleUrl: 'login.component.scss',
 })
 export default class LoginComponent {
-  credentials = {
-    email: '',
-    password: '',
-  };
+  private credentialsService = inject(LoginService);
+  private router = inject(Router);
+  private nfb = inject(NonNullableFormBuilder);
+
+  isSubmitted = false;
   isLoading = false;
   error = '';
 
-  private loginService = inject(LoginService);
-  private router = inject(Router);
+  credentials = this.nfb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  get email() {
+    return this.credentials.get('email');
+  }
+
+  get password() {
+    return this.credentials.get('password');
+  }
+
+  onErrorClick(): void {
+    this.error = '';
+  }
 
   onSubmit(): void {
+    this.isSubmitted = true;
+
+    if (!this.credentials.valid) {
+      this.credentials.markAllAsTouched();
+      return;
+    }
+
+    const { email, password } = this.credentials.getRawValue();
+
     this.isLoading = true;
 
-    const { email, password } = this.credentials;
-
-    this.loginService
+    this.credentialsService
       .signIn(email, password)
       .pipe(
         catchError((error: HttpErrorResponse) => {
