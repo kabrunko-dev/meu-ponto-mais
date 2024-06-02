c0d7e3b0f5297fd75c8b276a8b8056a826a56d15
kabrunko-dev <kabrunko.dev@gmail.com>
2024-06-02 10:14:53 -0300

    refactor(feat/my-records): wip update view integration

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 The chosen icons set was "Material Symbols / Material Icons" [git](https://github.com/google/material-design-icons).
 
 We can see all icons available [here](https://github.com/google/material-design-icons).
+
+## Ideas
+
+- Create component and service for global loading
+
+> It would be the only loading service for this state in the application
+
+- Make a simple cache for some requests
+
+> For example, `session` endpoint which data don't change or get updated often
--- src/app/features/my-records/my-records.component.ts
+++ src/app/features/my-records/my-records.component.ts
@@ -1,17 +1,49 @@
-import {Component} from "@angular/core";
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgFor, NgIf, SlicePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
-import CardComponent from "../../shared/components/card.component";
-import {NgFor} from "@angular/common";
+import { WorkDaysService } from '../board/services';
+import CardComponent from '../../shared/components/card.component';
+import { ClockPipe } from '../board/pipes';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-my-records',
   standalone: true,
-  imports: [CardComponent, NgFor],
+  imports: [
+    AsyncPipe,
+    CardComponent,
+    ClockPipe,
+    NgFor,
+    NgIf,
+    RouterLink,
+    SlicePipe,
+  ],
   template: `
-    <h3>Meu Ponto</h3>
+    <header>
+      <a routerLink="/board" type="button">
+        <span class="material-icons-outlined"> arrow_back </span>
+      </a>
+      <h3>Meu Ponto</h3>
+    </header>
 
-    @for (value of mock; track value) {
-      <app-card>{{ value }}</app-card>
+    @for (value of mock$ | async; track value.id) {
+      <app-card>
+        <div class="card__header">
+          <p>{{ value.date }}</p>
+          @if (value.time_cards.length > 0) {
+            <p>
+              {{ value.time_cards | slice: 0 : 1 }} -
+              {{ value.time_cards | slice: -1 }}
+            </p>
+          }
+        </div>
+        <div style="display: flex; gap 16px; justify-content: space-between;">
+          <p>{{ value.extra_time | clock }}</p>
+          <p>{{ value.missing_time | clock }}</p>
+          <p>{{ value.time_balance | clock }}</p>
+        </div>
+      </app-card>
     }
   `,
   styles: `
@@ -25,8 +57,47 @@ import {NgFor} from "@angular/common";
     h1 {
       margin-bottom: 16px;
     }
-  `
+
+    header {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+
+    .card__header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .material-icons-outlined {
+      line-height: 1.5;
+    }
+  `,
 })
 export default class MyRecordsComponent {
-  mock = Array(10).fill(null).map((_, index) => index);
+  private workDaysService = inject(WorkDaysService);
+
+  mock$ = new Observable<any>();
+
+  constructor() {
+    const now = new Date();
+    const twoDaysBefore = new Date(now);
+    twoDaysBefore.setDate(twoDaysBefore.getDate() - 15);
+
+    this.mock$ = this.workDaysService
+      .getTimeCardControl(
+        twoDaysBefore.toISOString().split('T')[0],
+        now.toISOString().split('T')[0]
+      )
+      .pipe(
+        map((value) =>
+          value.map((v: any) => ({
+            ...v,
+            date: v.date.split('-').reverse().join('/'),
+            time_cards: v.time_cards.map((tc: any) => tc.time),
+          }))
+        )
+      );
+  }
 }
